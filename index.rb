610696305156da787ac41bb31a6f1c8dc4ba0b49
Ikenna Richard <oguejioforrichard@gmail.com>
2023-06-16 15:15:13 -1100

    Fix linter issues

--- index.rb
+++ index.rb
@@ -2,13 +2,12 @@ require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
-  
+
   def initialize(*params)
     @list = params
   end
 
-  def each
-    @list.each { |item| yield item }
+  def each(&block)
+    @list.each(&block)
   end
-
 end
--- mylist_test.rb
+++ mylist_test.rb
@@ -1,20 +1,19 @@
 require_relative 'index'
-require 'rspec'
 
-RSpec.describe 'Tests for my list' do
+describe 'Tests for my list' do
   list = MyList.new(1, 2, 3, 4)
 
   it 'returns true/false based on the condition' do
-    expect(list.all? { |e| e < 5}).to be true
-    expect(list.all? { |e| e == 5}).to be false
+    expect(list.all? { |e| e < 5 }).to be true
+    expect(list.all? { |e| e == 5 }).to be false
   end
 
   it 'returns true/false based on the condition' do
-    expect(list.any? { |e| e == 2}).to be true
-    expect(list.any? { |e| e == 5}).to be false
+    expect(list.any? { |e| e == 2 }).to be true
+    expect(list.any? { |e| e == 5 }).to be false
   end
 
   it 'returns filtered list' do
-    expect(list.filter {|e| e.even?}).to eq([2, 4])
+    expect(list.filter(&:even?)).to eq([2, 4])
   end
 end
